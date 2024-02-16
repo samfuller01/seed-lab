09e488ea83d9f894c760265ae56592becc4db9d8
Ethan <ethankunze6@gmail.com>
2024-02-16 11:43:31 -1000

    Final

--- python/mini.py
+++ python/mini.py
@@ -7,7 +7,6 @@ import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 from smbus2 import SMBus
 import threading
 import queue
-from random import random
 
 #initialize lcd
 lcd_columns = 16
@@ -29,24 +28,30 @@ camera = cv2.VideoCapture(0)
 q = queue.Queue()
 
 def createString(number):
-    string = "Goal Position: "
+    string = "Goal Pos: "
     if number == 0:
         string = string + "0 0"
+        print("NE")
     elif number == 1:
         string = string + "0 1"
+        print("NW")
     elif number == 2:
         string = string + "1 1"
+        print("SW")
     elif number == 3:
         string = string + "1 0"
-    q.put(string)
+        print("SE")
+
+    return string
+
+def disp():
+    while True:
+        if not q.empty():
+            gotSomething = q.get()
+            q.queue.clear()
+            string = createString(gotSomething)
+            lcd.message = string
     
-def display():
-    if not q.empty():
-        disp = str(q.get())
-        lcd.write = disp
-        
-myThread = threading.Thread(target=display,args=())
-myThread.start()
 # Let the camera warmup
 sleep(0.1)
 
@@ -57,10 +62,12 @@ i2c = SMBus(1)
 offset = 1;
 command = 0;#stores quadrant markers is in
 #0-NE 1-NW 2-SW 3-SE
+myThread = threading.Thread(target=disp,args=())
+myThread.start()
+command = -1
 while(True):
     ret, image = camera.read()
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #convert image to grayscale
-    cv2.imshow('Image',gray)
     k = cv2.waitKey(1) & 0xFF
     #if key pressed exit loop
     if k == ord('q'):
@@ -68,11 +75,40 @@ while(True):
     #creates array of XY coordinates for each corner of marker, an array of the 
     #markers id and an array of each rejected canidate center of image. 
     corners,ids,rejected = aruco.detectMarkers(gray,aruco_dict)
-    #if aruco detected print out id else print none found. 
+    #if aruco detected print out id else print none found.
+    height, width, channels = image.shape
+    image = cv2.line(gray, (0,int(height/2)), (width, int(height/2)),(255,255,255),9)
+    image = cv2.line(gray, (int(width/2),0), (int(width/2),height),(255,255,255),9)
+    cv2.imshow('Image',gray)
     if not ids is None:
-        command = 3 #reads quadrant of markers in
-        i2c.write_byte_data(ARD_ADDR,offset,command)
-        createString(command)
+        markerCorners = corners[0][0]
+        centerX = markerCorners[0][0] + markerCorners[1][0] + markerCorners[2][0] + markerCorners[3][0]
+        centerY = markerCorners[0][1] + markerCorners[1][1] + markerCorners[2][1] + markerCorners[3][1]
+
+        centerX /= 4
+        centerY /= 4
+
+        absCenX = width/2
+        absCenY = height/2
+
+        xLeft = centerX < absCenX
+        yUp = centerY < absCenY
+
+       
+        temp = command #stores previous command
+        if xLeft and yUp:
+            command = 1
+        elif (not xLeft) and yUp:
+            command = 0
+        elif (not xLeft) and (not yUp):
+            command = 3
+        elif xLeft and (not yUp):
+            command = 2
+
+    
+        if command != temp:    
+            i2c.write_byte_data(ARD_ADDR,offset,command)
+            q.put(command)
         
 camera.release()
 cv2.destroyAllWindows()
