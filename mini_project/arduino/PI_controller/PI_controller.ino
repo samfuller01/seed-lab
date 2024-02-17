f2ca6becc4907ebe365d957c860df4786d2e8312
Sam Fuller <spfuller01@gmail.com>
2024-02-16 21:03:21 -0700

    cleaning up repo

--- mini_project/arduino/PI_controller/PI_controller.ino
+++ mini_project/arduino/PI_controller/PI_controller.ino
@@ -1,9 +1,14 @@
 /*
 * Name: Sam Fuller & Eugene Lee
 * Assignment: mini project
-* Description:
+* Description: When a message is received from the PI via I2C rotate the correct motor
+*              180 degrees depending on which command is received
+* Note: Integer value and corresponding quadrant
+* 0: NE, 1: NW, 2: SW, 3: SE
 */
-#include <Wire.h>
+#include <Wire.h> // I2C Library
+
+/******** Pin & Address Defines ********/
 
 #define M1A 2 // motor 1 encoder
 #define M2A 3 // motor 2 encoder
@@ -16,31 +21,32 @@
 #define M2PWR 10 // motor 2 speed input (voltage)
 #define I2C_ADDR 11 // I2C address
 
+/******** I2C Global Variables ********/
+
 // I2C variables
 volatile uint8_t offset = 0;
-volatile uint8_t instruction[10] = {0};
+volatile uint8_t instruction[1] = {0};
 volatile uint8_t msgLength = 0;
 volatile uint8_t reply[10] = {0};
 
-// Integer value and corresponding quadrant
-// 0: NE, 1: NW, 2: SW, 3: SE
+/******** Motor & Timer Global Variables ********/
 
 // motor control variables
 const unsigned long desired_time_us = 10000; // desired sample time in us (10 ms)
 const float battery_voltage = 7.8; // motor power supply
-const float Kp = 2; // system gain
-float Kp_pos[2] = {5, 3.5};
-const float Ki_pos = 0.7;
+const float Kp = 2; // velocity gain
+float Kp_pos[2] = {5, 3.5}; // P controller gain, unique for each motor
+const float Ki_pos = 0.7; // I controller gain
 unsigned long last_time_us, start_time_us; // timer stuff
 float current_time_ms = 0; // more timer stuff
-bool finished = false; // for printing finished out to MatLab
-unsigned long last_read_time_motors_us[2];
+//bool finished = false; // for printing finished out to MatLab - not used currently
+unsigned long last_read_time_motors_us[2] = {0, 0}; // stores the last time the motors were read
 long motor_counts_p[2] = {0, 0}; // private motor counts
-long motor_counts_pos[2] = {0, 0}; // public motor counts to be adjusted
+long motor_counts_pos[2] = {0, 0}; // public motor counts to be calc'd with
 float last_pos_rad[2] = {0, 0}; // previous position in radians
 float voltage[2] = {0, 0}; // voltage to motors
 float error[2] = {0, 0}; // error of feedback loop
-float pos_error[2] = {0, 0}; // error of position loop
+float pos_error[2] = {0, 0}; // error of position section
 float integral_error[2] = {0, 0}; // error of integral section
 float desired_pos_rad[2] = {0, 0}; // desired position of wheel
 float actual_pos_rad[2] = {0, 0}; // current position in radians
@@ -48,8 +54,10 @@ float desired_velocity_rad_s[2] = {0, 0}; // desired velocity in rad/s
 float actual_velocity_rad_s[2] = {0, 0}; // current velocity in rad/s
 unsigned int pwm[2] = {0, 0}; // PWM applied to motors
 
+/******** Motor Encoder Helper Functions ********/
+
 // encoder ISR from assignment 1
-void motor_one_encoder_isr() {
+void motor_one_encoder_isr(void) {
   if (micros() - last_read_time_motors_us[0] > 100) {
     if (digitalRead(M1A) == digitalRead(M1B)) {
       motor_counts_p[0] -= 2;
@@ -60,7 +68,7 @@ void motor_one_encoder_isr() {
   }
 }
 
-void motor_two_encoder_isr() {
+void motor_two_encoder_isr(void) {
   if (micros() - last_read_time_motors_us[1] > 100) {
     if (digitalRead(M2A) == digitalRead(M2B)) {
       motor_counts_p[1] -= 2;
@@ -82,8 +90,10 @@ long encoder(const int motor) {
   }
 }
 
+/******** I2C Helper Functions ********/
+
 // function called when an I2C interrupt event happens
-void receive() {
+void receive(void) {
   // Set the offset, this will always be the first byte.
   offset = Wire.read();
   // If there is information after the offset, it is telling us more about the command.
@@ -95,14 +105,23 @@ void receive() {
   }
 }
 
-// function to request info from I2C
-void request() {
+// function to request info from I2C - not used currently
+void request(void) {
   // According to the Wire source code, we must call write() within therequesting ISR
   // and nowhere else. Otherwise, the timing does not work out. See line238:
   // https://github.com/arduino/ArduinoCore-avr/blob/master/libraries/Wire/src/Wire.cpp
   msgLength = 0;
   
   Wire.write(reply[msgLength]); 
+}
+
+// printReceived helps us see what data we are getting from the leader
+void printReceived(void) {
+// Print on serial console
+  Serial.print("Instruction received: ");
+  for (int i=0;i<msgLength;i++) {
+    Serial.print(String(instruction[i])+"\t");
+  }
 }
 
 void setup() {
@@ -119,11 +138,11 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(M1A), motor_one_encoder_isr, CHANGE);
   attachInterrupt(digitalPinToInterrupt(M2A), motor_two_encoder_isr, CHANGE);
   Serial.begin(115200); // high baud rate to zoom
+  // I2C config
   Wire.begin(I2C_ADDR);
   Wire.onReceive(receive);
   Wire.onRequest(request);
   Serial.println("Ready!"); // MatLab ready up
-  delay(2000); // for testing motor rotation
 }
 
 void loop() {
@@ -149,8 +168,6 @@ void loop() {
     }
     msgLength = 0;
   }
-  //desired_pos_rad[0] = PI; // desired position for each wheel
-  //desired_pos_rad[1] = PI;
 
   motor_counts_pos[0] = encoder(1); // get motor counts
   motor_counts_pos[1] = encoder(2);
@@ -165,14 +182,15 @@ void loop() {
 
   // motor controllers
   for (int i = 0; i < 2; i++) {
-    pos_error[i] = desired_pos_rad[i] - actual_pos_rad[i];
-    integral_error[i] = integral_error[i] + pos_error[i] * ((float)desired_time_us / 1000000);
-    desired_velocity_rad_s[i] = Kp_pos[i] * pos_error[i] + Ki_pos * integral_error[i];
-    error[i] = desired_velocity_rad_s[i] - actual_velocity_rad_s[i];
-    voltage[i] = Kp * error[i];
+    pos_error[i] = desired_pos_rad[i] - actual_pos_rad[i]; // position feedback loop, calculating error
+    integral_error[i] = integral_error[i] + pos_error[i] * ((float)desired_time_us / 1000000); // calculating integral error
+    desired_velocity_rad_s[i] = Kp_pos[i] * pos_error[i] + Ki_pos * integral_error[i]; // getting desired velocity using PI controller
+    error[i] = desired_velocity_rad_s[i] - actual_velocity_rad_s[i]; // getting error for voltage
+    voltage[i] = Kp * error[i]; // getting voltage using gain and error
+    pwm[i] = 255 * abs(voltage[i]) / battery_voltage; // set motor PWM
   }
 
-  // set motor directions
+  // set motor directions depending on motor voltage sign
   if (voltage[0] > 0) {
     digitalWrite(M1DIR, HIGH);
   } else {
@@ -184,13 +202,10 @@ void loop() {
     digitalWrite(M2DIR, LOW);
   }
 
-  pwm[0] = 255 * abs(voltage[0]) / battery_voltage; // set motor PWMs
-  pwm[1] = 255 * abs(voltage[1]) / battery_voltage;
-
-  analogWrite(M1PWR, min(pwm[0], 255)); // write PWM to motor
+  analogWrite(M1PWR, min(pwm[0], 255)); // write PWM to both motors
   analogWrite(M2PWR, min(pwm[1], 255));
 
-  // print out status variables
+  // print out status variables forever
   Serial.print(current_time_ms);
   Serial.print("\t");
   Serial.print(desired_pos_rad[0]);
@@ -208,42 +223,10 @@ void loop() {
   Serial.print(actual_pos_rad[0]);
   Serial.print("\t");
   Serial.println(actual_pos_rad[1]);
-  
-  /*if (!finished) {
-  }
-  if (current_time_ms > 3000) { // stop after 3 seconds
-    desired_velocity_one_rad_s = 0;
-    desired_velocity_one_rad_s = 0;
-    if (!finished) {
-      finished = true;
-      Serial.println("Finished");
-    }
-    analogWrite(M1PWR, 0);
-    analogWrite(M2PWR, 0);
-  }*/
 
   while (micros() < last_time_us + desired_time_us); // sample rate things
   last_time_us = micros(); // set comparison variables
+  // setting previous variables for calculating delta in feedback loop
   last_pos_rad[0] = actual_pos_rad[0];
   last_pos_rad[1] = actual_pos_rad[1];
-}
-
-//void loop() {
-//  // If there is data on the buffer, read it
-//  if (msgLength > 0) {
-//    if (offset==1) {
-//      digitalWrite(LED_BUILTIN,instruction[0]);
-//    }
-//  printReceived();
-//  msgLength = 0;
-//  }
-//}
-
-// printReceived helps us see what data we are getting from the leader
-void printReceived() {
-// Print on serial console
-  Serial.print("Instruction received: ");
-  for (int i=0;i<msgLength;i++) {
-    Serial.print(String(instruction[i])+"\t");
-  }
 }
